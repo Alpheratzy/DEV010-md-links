ac6fc652505caaa4ab71fce51420dfd5836eee62
Alpheratzy <alexandraronvera@gmail.com>
2023-11-13 14:07:54 -0300

    termminando el hito 2

--- resources/functions/validator.js
+++ resources/functions/validator.js
@@ -1,12 +1,33 @@
-function validator(linksToComplete){
+/*function validator(linksToComplete){
 
     const validateArray = linksToComplete.map(link => {
         link.status = "prueba";
         link.ok = "ok";
 
-        return link;
+        return link
     });
     return validateArray;
 }
+*/
 
+function validator(linksToComplete) {
+    const validateArray = linksToComplete.map(link => {
+      return fetch(link.url)
+        .then(response => {
+          link.status = response.status;
+          link.ok = response.ok;
+          return link;
+        })
+        .catch(error => {
+          // Manejar errores de la solicitud
+          console.error('Error en la solicitud:', error);
+          link.status = 'Error';
+          link.ok = false;
+          return link;
+        });
+    });
+  
+    return Promise.all(validateArray);
+  }
+  
 module.exports = validator;
